f3ad76d80c9a5dd8c591f9a3c14ee8162425a600
sunchen <1303220915@qq.com>
2023-11-15 09:49:04 +0800

    修复一些代码错误，小改动

--- src/NexttraceTool.js
+++ src/NexttraceTool.js
@@ -1,5 +1,6 @@
 const { spawn} = require('child_process');
 const { MongoClientTool } = require('./MongoClient')
+const fs = require('fs')
 class NexttraceTool {
     constructor() {
         this.mongoClient = new MongoClientTool('fun', 'task')
@@ -7,10 +8,11 @@ class NexttraceTool {
 
     async getRoute (host) {
         const orders = ['nexttrace', '-j', '-M', host]
-        let content = ''
+        let content = Buffer.from('')
         const mtrProcess = spawn('sudo', orders);
         mtrProcess.stdout.on('data', (data) => {
-            content += data
+            console.log('=======>', data.toString('utf-8'))
+            content = Buffer.concat([content, data])
         })
 
         mtrProcess.stderr.on('data', (data) => {
@@ -19,7 +21,9 @@ class NexttraceTool {
 
         mtrProcess.on('close', (code) => {
             console.log('close------>')
-            console.log(content)
+
+            fs.writeFileSync('./text.json', content.toString('utf-8'), { encoding: 'utf-8' })
+            console.log(content.toString('utf8'))
             console.log(`mtr process exited with code ${code}`);
         });
     }
